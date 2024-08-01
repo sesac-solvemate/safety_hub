391f14a16dd2f723c1220c2d6d5c4d2a5e18ad07
leessunj <sj06152@gmail.com>
2024-08-01 20:37:40 +0900

    processing: data추가 및 API 구현 중

---
+++ backend/constant.py
@@ -0,0 +1,42 @@
+class INDUSTRY:
+    CODE="code"
+    NAME="name"
+    MID_CATEGORY="midCategory"
+    SUB_CATEGORY="subCategory"
+
+class RESOURCE:
+    STEP="step"
+    LAST_TIME="lastTime"
+    COMPANY_NAME="companyName"
+    INDUSTRY_CODE="industryCode"
+    COMPANY_FILE="companyFileUrl"
+    TOPIC_TYPE="topicType"
+    TOPIC_ID="topicId"
+
+class PROCESS:
+    INDUSTRY_CODE="industryCode"
+    PROCESS_ID="id"
+    PROCESS_NAME="name"
+    PROCESS="process"
+
+class EQUIP:
+    INDUSTRY_CODE="industryCode"
+    EQUIP_ID="id"
+    EQUIP_NAME="name"
+    EQUIP="process"
+
+class RISK:
+    GUIDE_ID="guideId"
+    PROCESS_ID="processId"
+    RISK="risk"
+    OWN_RISK="ownRisk"
+    COMPANY_RISK="companyRisk"
+
+class GUIDE:
+    LANGUAGE="language"
+    LANGUAGE_KOR="ko"
+    LANGUAGE_ENG="en"
+    LANGUAGE_CHE="ch"
+    LANGUAGE_CHEF="vi"
+    IMAGE="images"
+    DOWNLOAD_URL="downloadUrl"
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
-import datetime
 import io
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
@@ -9,30 +7,185 @@ from PyPDF2 import PdfReader
 from pyhtml2pdf import converter
 import pymongo
 import uuid
-from typing import Annotated
+from typing import Annotated,Optional
 import numpy as np
 import pandas as pd
-
-app=FastAPI()
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+from datetime import datetime
+import json
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    print(file.content_type)
+from constant import PROCESS,RISK,EQUIP,RESOURCE,GUIDE,INDUSTRY
+
+# load .env
+load_dotenv()
+app = FastAPI()
+
+# MongoDB 설정
+MONGO_USER = os.environ.get('MONGO_DB_ROOT_USER_NAME')
+MONGO_PASSWORD = os.environ.get('MONGO_DB_ROOT_USER_PASSWORD')
+MONGO_HOST = os.environ.get('MONGO_DB_HOST', 'localhost')
+MONGO_PORT = os.environ.get('MONGO_DB_PORT', '27017')
+print(MONGO_USER,MONGO_PASSWORD,MONGO_HOST,MONGO_PORT)
+client = pymongo.MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/safetyhub?authSource=admin&retryWrites=true&w=majority")
+db = client["safetyhub"]
+collection = db["timestamps"]
+print(db)
+print(collection)
+
+
+
+def updateDatetime():
+    current_time = datetime.utcnow()
+    print(current_time)
+    collection.insert_one({"timestamp": current_time})
+    return {"message": "Timestamp added", "time": current_time}
+
+@app.get("/upload/{databaseName}")
+async def upload_json_to_database(databaseName):
+    # 현재 파일의 절대 경로
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # 상대 경로를 사용하여 파일 경로 구성
+    file_path = os.path.join(current_dir, '..', 'data', f'{databaseName}.json')
+    print(file_path)
+    with open(file_path, "r", encoding='utf-8') as f:
+        data = json.load(f)
+        db[databaseName].insert_many(data)
+        print(data)
+    return {"file_size": "good"}
+
+
+
+@app.get("/guide")
+async def createGuideId():
+    print("createGid")
+    guideId = str(uuid.uuid1())
+    db = client["safetyhub"]
+    print("DB")
+    collection = db['resources']
+    print("COlelction")
+    collection.insert_one({
+        "_id":guideId,
+        "step":1
+    })
+    return guideId
+
+@app.get("/guide/{guideId}")
+async def getGuideData(guideId):
+    collection = db['resources']
+    data = collection.find_one({"_id": guideId},
+                           {"_id": 0, "step": 1, "lastTime": 1})
+    print("guideId",data)
+    return data
+
+def findIndustryName(industryCode):
+    # 컬렉션 선택
+    collection = db['industry']
+
+    majCode=industryCode//1000
+    # 특정 문서 가져오기 (예를 들어, code가 3인 문서)
+    document = collection.find_one({"code": majCode})
+
+    # subCategory의 name 찾기
+    sub_category_name = None
+
+    # subCategory 필드 추출
+    if document:
+        mid_categories = document.get("midCategory", [])
+        for mid_category in mid_categories:
+            sub_categories = mid_category.get("subCategory", [])
+            for sub_category in sub_categories:
+                if sub_category.get("code") == industryCode:
+                    sub_category_name = sub_category.get("name")
+                    break
+            if sub_category_name:
+                break
+    if sub_category_name:
+        return sub_category_name
+    else:
+        return None #ERROR
+
+
+@app.get("/guide/{guideId}/{step}")
+async def getStepDataFromGuideId(guideId, step:int):
+    collection=db['resources']
+    result={}
+    if step == 1:
+        data = collection.find({ "_id" : guideId},
+                                  {"_id": 0, "companyName": 1, "industryCode": 1, "companyFileUrl": 1})
+        result["companyName"]=data["companyName"]
+        result["industryCode"]=findIndustryName(data["industryCode"])
+        result["companyFileUrl"] = data["companyFileUrl"]
+    elif step == 2:
+        data = collection.find({"_id": guideId},
+                                  {"_id": 0, "topicType": 1, "topicId": 1})
+        result=data
+    elif step == 3: #정해야함
+        data = collection.find({"_id": guideId},
+                                   {"_id": 0, "companyName": 1, "industryCode": 1, "companyFileUrl": 1})
+        pass
+    elif step == 4:
+        collection=db["guide"]
+        data = collection.find_one({"_id": guideId})
+        if data:
+            return data
+        else:
+            return {}
+    else:
+        return {"error": "step not valid"}
+        # query_filter = {"author":"sunjae"}
+        # update_operation = {"$set":
+        #                         {"<field name>": "sunj2-value"}
+        #                     }
+        # result = collection.update_many(query_filter, update_operation)
+
+    return result
+
+@app.get("/industry")
+async def getIndustry():
+    results=db.industry.find({},{"_id": 0})
+    industry=[]
+    for x in results:
+        industry.append(x)
+    print(industry)
+    return industry
+
+class Data1(BaseModel):
+    companyName: str
+    industryCode: int
+    companyFile : UploadFile | None = None
+
+class Data2(BaseModel):
+    topicType: str
+    topic: int
+
+class Data3E(BaseModel):
+    warnings: str
+
+@app.get("/step")
+async def stepbystep():
+    print("OK")
+    return {"step":1}
+
+# step 데이터 저장
+@app.post("/step/{guideId}/1")
+async def saveStepData(guideId, companyName: str=Form(...),
+    industryCode: int=Form(...),
+    companyFile : Optional[UploadFile] = File(None)):
+    print("helo step1")
+    print(companyName,industryCode)
+    if companyFile:
+        print(companyFile.content_type)
     # if file.content_type not in ["application/vnd.ms-excel",
     #                              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]:
     #     return {"error": "Invalid file type. Please upload an Excel file."}
 
     try:
         # 파일 내용을 읽어 BytesIO 객체로 변환
-        contents = await file.read()
+        contents = await companyFile.read()
         data = io.BytesIO(contents)
 
         # pandas를 사용하여 Excel 파일 읽기
-        df_dict = pd.read_excel(data,sheet_name=None)
+        df_dict = pd.read_excel(data, sheet_name=None)
         print(df_dict["3-3 위험성평가(1)"])
         # 데이터 확인 (예시: 데이터프레임의 첫 5행 출력)
         # data_preview = {sheet: df.head().to_dict() for sheet, df in df_dict.items()}
@@ -44,102 +197,31 @@ async def create_upload_file(file: UploadFile):
 
     except Exception as e:
         return {"error": str(e)}
-    return {"filename": file.filename,"keys":list(df_dict.keys())}
-def htmlToPdf(htmlFile):
-    path = os.path.abspath('html_test.html')
-    print(path)
-    converter.convert(f'file:///{path}', 'sample2.pdf')
-    # converter.convert('https://pypi.org', 'sample.pdf')
-
-def pdfToText(pdfFile):
-    reader = PdfReader("example.pdf")
-    number_of_pages = len(reader.pages)
-    page = reader.pages[0]
-    text = page.extract_text()
-    # print(reader)
-    # print(page)
-    print(text)
-
-
-
-# load .env
-load_dotenv()
-
-MONGO_USER = os.environ.get('MONGO_DB_ROOT_USER_NAME')
-MONGO_PASSWORD = os.environ.get('MONGO_DB_ROOT_USER_PASSWORD')
-
-
-client=pymongo.MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017/safetyhub?authSource=admin")
-
-class Item(BaseModel):
-    name: str
-    type: str | None = None
-
-@app.get("/")
-async def first():
-    print("hi first")
-    # make_html()
-
-    db = client["safetyhub"]
-    # collection_list = db.list_collection_names()
-    # for c in collection_list:
-    #     print(c)
-    collection = db['mycol']
-    print(client)
-    print(client.get_database())
-    print(db)
-    print(collection)
-
-    # query_filter = {"author":"sunjae"}
-    # update_operation = {"$set":
-    #                         {"<field name>": "sunj2-value"}
-    #                     }
-    # result = collection.update_many(query_filter, update_operation)
-
-    results = collection.find({"<field name>":"sunj2-value"})
-    print(results)
-    print(list(results))
-    for document in results:
-        print(document)
-
-    # print(db.mycol.insert_one({
-    #     "_id":"sunjaehaha",
-    #     "author":"jason"
-    # }).inserted_id)
-
-
-
-
-@app.post("/guide")
-async def createGuideId():
-    guideId = str(uuid.uuid1())
-    # save UUID->collections
-    return guideId
-
-@app.get("/guide/{guideId}")
-async def getGuide(guideId):
-
-    return {
-        "step": 2,
-        "lastTime": datetime.datetime.now()
-    }
-
-@app.get("/guide/{guideId}/{step}")
-async def getStepDataFromGuideId(guideId, step:int):
-    return {
-
-    }
-
-@app.get("/industry")
-async def getIndustry():
-    return []
+    return {"filename": companyFile.filename, "keys": list(df_dict.keys())}
 
 @app.post("/step/{guideId}/{step}")
-async def saveStepData(guideId, step:int):
-    pass
+async def saveStepData(guideId, step:int,data: Data2 | Data3E):
+    print("helo step")
+    if step == 2:
+        pass
+    elif step == 3:
+        pass
+    elif step == 4:
+        pass
+    else:
+        return {"error":"step not valid"}
+
 
 @app.get("/process/{guideId}")
 async def getProcessList(guideId):
+    guideData=db.resources.find_one({"_id": guideId},{"industryCode":1})
+    if guideData and "industryCode" in guideData:
+        defaultData=db.process.find({"industryCode":guideData["industryCode"]})
+        ownData = db.process.find({}, {"_id": 0})
+        # process = []
+        # for x in results:
+        #     process.append(x)
+        # print(process)
     return []
 
 @app.get("/equipment/{guideId}")
@@ -155,16 +237,17 @@ async def getSelectedRiskList(guideId):
     return []
 
 @app.post("/risk")
-async def createRiskData():
+async def createRiskData(riskName):
+
     pass
 
 @app.put("/risk/{guideId}")
 async def updateSelectedRisk(guideId):
     pass
 
-@app.delete("/risk/{guideId}")
-async def deleteRisk(guideId):
-    pass
+# @app.delete("/risk/{guideId}")
+# async def deleteRisk(guideId):
+#     pass
 
 @app.get("/risk/")
 async def getRiskListByKeyword(keyword):
@@ -189,14 +272,15 @@ async def getSafetyGuideWithLanguage(guideId, language="ko"):
         pass
     return {}
 
-@app.put("/summary/{guideId}/")
-async def updateOnlySafetyGuideWithLanguage(guideId, language="ko"):
-    if language not in SUPPORT_LANGUAGE:
-        pass
-    return {}
+# @app.put("/summary/{guideId}/")
+# async def updateOnlySafetyGuideWithLanguage(guideId, language="ko"):
+#     if language not in SUPPORT_LANGUAGE:
+#         pass
+#     return {}
 
 @app.get("/download/{guideId}")
 async def download(guideId):
+
     return ""
 
 
@@ -205,3 +289,18 @@ def make_html():
 
     file.write("<html><head><title>테스트 타이틀</title></head><body>테스트 입니다. </body></html>")
     file.close()
+
+def htmlToPdf(htmlFile):
+    path = os.path.abspath('html_test.html')
+    print(path)
+    converter.convert(f'file:///{path}', 'sample2.pdf')
+    # converter.convert('https://pypi.org', 'sample.pdf')
+
+def pdfToText(pdfFile):
+    reader = PdfReader("example.pdf")
+    number_of_pages = len(reader.pages)
+    page = reader.pages[0]
+    text = page.extract_text()
+    # print(reader)
+    # print(page)
+    print(text)
