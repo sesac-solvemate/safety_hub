3023a0900680eb2bb8445b92706a0ef1cdfdc993
nayoung98 <gilony@naver.com>
2024-08-05 16:27:35 +0900

    test: generation, translation 테스트 코드 추가

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from engine import *
 
 label_list = ['conveyer', 'electricity', 'painting', 'chemistry', 'transportation', 'bone_disease', 'inversion', 'forklift1', 'forklift2', 'forklift3', 'crane', 'workplace', '끼임/깔림/부딪힘', '화재']
 
-path = '/home/ny/Safety_Hub/ai/prompt.json'
+path = './prompt.json'
 content = read_json(path)
 
 ############## Generate the context ##############
